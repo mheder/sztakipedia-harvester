81824177981c79f88b25ab0f0540164b73431ff1
farkast21@gmail.com <farkast21@gmail.com@de6975fe-1327-a506-18f7-aa93be42de21>
2012-06-19 14:33:49 +0000

    Work in progress: LuceneConsumer (replaces CasToLucene) and LuceneIndexer (replaces Indexer) getDocument() for searcher

--- trunk/hu.sztaki.pedia.harvester/src/hu/sztaki/pedia/lucene/LuceneIndexer.java
+++ trunk/hu.sztaki.pedia.harvester/src/hu/sztaki/pedia/lucene/LuceneIndexer.java
@@ -1,15 +1,23 @@
 package hu.sztaki.pedia.lucene;
 
+import hu.sztaki.pedia.lucene.exceptions.BadQueryException;
+import hu.sztaki.pedia.lucene.exceptions.NewerVersionStoredException;
 import hu.sztaki.pedia.lucene.util.IChainIndexerUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.NIOFSDirectory;
+import org.apache.uima.WikiArticleAnnotation;
+import org.apache.uima.cas.text.AnnotationIndex;
+import org.apache.uima.jcas.tcas.Annotation;
 
 public class LuceneIndexer {
 	private static final int COMMIT_THRESHOLD = 1000;
@@ -68,6 +76,37 @@ public class LuceneIndexer {
 
 	public static synchronized void setWriterInitialized(boolean writerInitialized) {
 		LuceneIndexer.writerInitialized = writerInitialized;
+	}
+
+	public void indexAnnotations(Map<Integer, AnnotationIndex<Annotation>> annotationIndexMap)
+			throws NewerVersionStoredException {
+		WikiArticleAnnotation wikiArticleAnnotation = (WikiArticleAnnotation) annotationIndexMap
+				.get(WikiArticleAnnotation.type).iterator().get();
+		if (isNewerVersionStored(wikiArticleAnnotation.getId(), wikiArticleAnnotation.getRevision())) {
+			throw new NewerVersionStoredException();
+		}
+		// TODO finish indexing
+
+	}
+
+	private boolean isNewerVersionStored(long id, long revisionID) {
+		boolean isStored = false;
+		try {
+			TopDocs result = searcher.simpleSearch(IndexFieldNames.ID_FIELD_NAME,
+					Long.toString(id), 1);
+			if (result.scoreDocs.length == 1) {
+				Document doc = searcher.getDocument(result.scoreDocs[0].doc);
+				long revFound = Long.parseLong(doc.get(IndexFieldNames.REVISION_FIELD_NAME));
+				if (revFound >= revisionID) {
+					isStored = true;
+				}
+			}
+		} catch (BadQueryException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return isStored;
 	}
 
 }
--- trunk/hu.sztaki.pedia.harvester/src/hu/sztaki/pedia/uima/consumer/LuceneConsumer.java
+++ trunk/hu.sztaki.pedia.harvester/src/hu/sztaki/pedia/uima/consumer/LuceneConsumer.java
@@ -2,6 +2,7 @@ package hu.sztaki.pedia.uima.consumer;
 
 import hu.sztaki.pedia.lucene.Indexer;
 import hu.sztaki.pedia.lucene.LuceneIndexer;
+import hu.sztaki.pedia.lucene.exceptions.NewerVersionStoredException;
 import hu.sztaki.pedia.lucene.util.EngPosChainIndexerUtil;
 import hu.sztaki.pedia.lucene.util.HunPosChainIndexerUtil;
 import hu.sztaki.pedia.lucene.util.HunStemChainIndexerUtil;
@@ -9,12 +10,24 @@ import hu.sztaki.pedia.lucene.util.IChainIndexerUtil;
 import hu.sztaki.pedia.uima.engines.AbstractMultiSofaAnnotator;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.index.CorruptIndexException;
+import org.apache.uima.SentenceAnnotation;
+import org.apache.uima.SourceDocumentInformation;
+import org.apache.uima.TokenAnnotation;
 import org.apache.uima.UimaContext;
+import org.apache.uima.WikiArticleAnnotation;
+import org.apache.uima.WikiCategoryAnnotation;
+import org.apache.uima.WikiLinkAnnotation;
+import org.apache.uima.WikiTemplateAnnotation;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.text.AnnotationIndex;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 
 public class LuceneConsumer extends AbstractMultiSofaAnnotator {
@@ -59,9 +72,8 @@ public class LuceneConsumer extends AbstractMultiSofaAnnotator {
 						unimportantPOSTags);
 			} else
 				throw new ResourceInitializationException();
-			// IndexWriterConfig iwc = chainIndexerUtil.getIndexWriterConfig();
 			indexer = LuceneIndexer.getInstance();
-			if (Indexer.isWriterInitialized()) {
+			if (!Indexer.isWriterInitialized()) {
 				indexer.initialize(indexDir, chainIndexerUtil);
 			}
 		} catch (IOException e) {
@@ -72,6 +84,32 @@ public class LuceneConsumer extends AbstractMultiSofaAnnotator {
 
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		ArrayList<JCas> casList = getJCasList(aJCas);
+		try {
+			for (JCas doc : casList) {
+				// put all annotation indexes into a map
+				Map<Integer, AnnotationIndex<Annotation>> annotationIndexMap = new HashMap<Integer, AnnotationIndex<Annotation>>();
+				annotationIndexMap.put(WikiArticleAnnotation.type,
+						doc.getAnnotationIndex(WikiArticleAnnotation.type));
+				annotationIndexMap.put(SourceDocumentInformation.type,
+						doc.getAnnotationIndex(SourceDocumentInformation.type));
+				annotationIndexMap.put(TokenAnnotation.type,
+						doc.getAnnotationIndex(TokenAnnotation.type));
+				annotationIndexMap.put(SentenceAnnotation.type,
+						doc.getAnnotationIndex(SentenceAnnotation.type));
+				annotationIndexMap.put(WikiLinkAnnotation.type,
+						doc.getAnnotationIndex(WikiLinkAnnotation.type));
+				annotationIndexMap.put(WikiTemplateAnnotation.type,
+						doc.getAnnotationIndex(WikiTemplateAnnotation.type));
+				annotationIndexMap.put(WikiCategoryAnnotation.type,
+						doc.getAnnotationIndex(WikiCategoryAnnotation.type));
+				// store the annotations in indexes, or exit if newer version
+				// already stored
+				indexer.indexAnnotations(annotationIndexMap);
+			}
+		} catch (NewerVersionStoredException e) {
+			aJCas.release();
+		}
 
 	}
 
